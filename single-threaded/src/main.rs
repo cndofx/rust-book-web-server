4b80ad9c21012453adcfeb4f36582e54154dea2d
cndofx <19335833+cndofx@users.noreply.github.com>
2022-12-18 02:08:04 -0700

    handle non "GET /" requests with a 404

--- single-threaded/src/main.rs
+++ single-threaded/src/main.rs
@@ -8,24 +8,25 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("connection established");
         handle_connection(stream);
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let reader = BufReader::new(&mut stream);
-    let request = reader
-        .lines()
-        .map(|line| line.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect::<Vec<_>>();
-    // println!("request: {request:#?}");
-
-    // let response = "HTTP/1.1 200 OK\r\n\r\n";
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = std::fs::read_to_string("../hello.html").unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
+    let request_line = reader.lines().next().unwrap().unwrap();
+    println!("received request: {request_line}");
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = std::fs::read_to_string("../hello.html").unwrap();
+        let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = std::fs::read_to_string("../404.html").unwrap();
+        let length = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
