c3389bd9f965b3fe8eb2398687540e7ee6f59f61
cndofx <19335833+cndofx@users.noreply.github.com>
2022-12-18 03:55:07 -0700

    handle requests asynchronously

--- async-tokio/src/main.rs
+++ async-tokio/src/main.rs
@@ -1,27 +1,35 @@
-use std::{
-    io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
-};
+// use std::{
+//     io::{BufRead, BufReader, Write},
+//     net::{TcpListener, TcpStream},
+// };
+
+use tokio::{net::{TcpListener, TcpStream}, io::{BufReader, AsyncBufReadExt, AsyncWriteExt}};
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream).await;
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
+    loop {
+        let (stream, _addr) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            println!("tokio task spawned");
+            handle_connection(stream).await;
+            println!("tokio task finished");
+        });
     }
 }
 
 async fn handle_connection(mut stream: TcpStream) {
+    // get request asynchronously
     let reader = BufReader::new(&mut stream);
-    let request_line = reader.lines().next().unwrap().unwrap();
+    let request_line = reader.lines().next_line().await.unwrap().unwrap();
     println!("received request: {request_line}");
 
+    // get response asynchronously
     let (status_line, filename) = match request_line.as_str() {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "../hello.html"),
         "GET /sleep HTTP/1.1" => {
             println!("sleeping...");
-            // std::thread::sleep(std::time::Duration::from_secs(5));
+            // non blocking sleep
             tokio::time::sleep(std::time::Duration::from_secs(5)).await;
             println!("done sleeping");
             ("HTTP/1.1 200 OK", "../hello.html")
@@ -32,6 +40,7 @@ async fn handle_connection(mut stream: TcpStream) {
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
+    // send response asynchronously
     println!("sending response: {status_line}");
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write_all(response.as_bytes()).await.unwrap();
 }
