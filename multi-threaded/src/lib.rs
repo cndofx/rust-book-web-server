029fb0f512c387dcdd13db448c107f3b67a5eeb5
cndofx <19335833+cndofx@users.noreply.github.com>
2022-12-18 02:48:02 -0700

    use channels to send jobs to worker threads

--- multi-threaded/src/lib.rs
+++ multi-threaded/src/lib.rs
@@ -1,37 +1,45 @@
-use std::thread::JoinHandle;
+use std::{sync::{mpsc, Arc, Mutex}, thread};
 
 pub struct ThreadPool {
     threads: Vec<Worker>,
+    sender: mpsc::Sender<Job>
 }
 
 struct Worker {
     id: usize,
-    handle: JoinHandle<()>,
+    handle: thread::JoinHandle<()>,
 }
+
+struct Job;
 
 impl ThreadPool {
     pub fn new(num_threads: usize) -> Result<Self, &'static str> {
         if num_threads < 1 {
             return Err("number of threads must be at least 1");
-        } 
-        let mut threads = Vec::with_capacity(num_threads);
-        for id in 0..num_threads {
-            threads.push(Worker::new(id))
         }
 
-        Ok(ThreadPool { threads })
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut threads = Vec::with_capacity(num_threads);
+        for id in 0..num_threads {
+            threads.push(Worker::new(id, receiver.clone()))
+        }
+
+        Ok(ThreadPool { threads, sender })
     }
 
-    pub fn execute<F: FnOnce() + Send + 'static>(&self, f: F) {
-
-    }
+    pub fn execute<F: FnOnce() + Send + 'static>(&self, f: F) {}
 }
 
 impl Worker {
-    fn new(id: usize) -> Self {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let handle = thread::spawn(|| {
+            receiver;
+        });
         Worker {
             id,
-            handle: std::thread::spawn(|| {}),
+            handle,
         }
     }
 }
