46052fb6718526d93df7847ca8e460f7e2dfd728
cndofx <19335833+cndofx@users.noreply.github.com>
2022-12-18 01:55:20 -0700

    read requests

--- single-threaded/src/main.rs
+++ single-threaded/src/main.rs
@@ -1,10 +1,24 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let sream = stream.unwrap();
-        println!("connection established")
+        let stream = stream.unwrap();
+        println!("connection established");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let reader = BufReader::new(&mut stream);
+    let request = reader
+        .lines()
+        .map(|line| line.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect::<Vec<_>>();
+    println!("request: {request:#?}");
 }
